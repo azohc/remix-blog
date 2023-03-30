ab1d5ea46867a0abaa803748ced6a0fe830db23d
jchozas <juanchozass@gmail.com>
2023-03-30 19:50:57 +0200

    plank

---
+++ app/components/Plank.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react"
+
+const transform =
+  "transform transitoverflow-hiddenion duration-500 motion-reduce:transition-none"
+const join = (classNames: string[]) => classNames.join(" ")
+
+export default function Plank() {
+  const [shifted, setShifted] = useState(true)
+
+  return (
+    <div
+      className={join([
+        "transition-[width] h-16 overflow-x-hidden relative cursor-pointer",
+        shifted ? "w-24" : "w-32",
+      ])}
+      onClick={() => setShifted(!shifted)}
+    >
+      <h1
+        className={join([
+          transform,
+          "absolute text-red-900".concat(
+            shifted ? " translate-x-full" : " translate-x-0"
+          ),
+        ])}
+        style={{ backfaceVisibility: "hidden" }}
+      >
+        chozas
+      </h1>
+      <h1
+        className={join([
+          transform,
+          "absolute text-blue-900".concat(
+            shifted ? " translate-x-0" : " -translate-x-full"
+          ),
+        ])}
+        style={{ backfaceVisibility: "hidden" }}
+      >
+        azohc
+      </h1>
+    </div>
+  )
+}
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,6 @@
 import { json } from "@remix-run/node"
 import { useLoaderData } from "@remix-run/react"
+import Plank from "~/components/Plank"
 import PostsCardList from "~/components/PostsCardList"
 import { getPosts } from "~/models/post.server"
 
@@ -14,8 +15,7 @@ export default function Index() {
   return (
     <main className="mx-auto mt-16 max-w-7xl flex flex-col items-center px-4">
       <header className="mb-16 self-start">
-        <h1>azohc.blog</h1>
-        <em>TODO interactive name graphic</em>
+        <Plank />
       </header>
       <h2 className="mb-16">
         lessons learnt on a journey into the world of{" "}

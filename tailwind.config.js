434cd3769babc8c2e7e8fac1e4a44de7180a4492
juan c <juanchozass@gmail.com>
2023-03-30 09:27:26 +0200

    base styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./app/**/*.{ts,tsx,jsx,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        jet: "#353535",
+        blueGray: "#5b6371",
+        gray: "#8A897C",
+        lightGray: "#BDBBB0",
+        lightBlueGray: "#D2D7DF",
+      },
+      fontFamily: {
+        sans: ["Inconsolata", "sans-serif"],
+        serif: ["Inconsolata", "serif"],
+      },
+    },
   },
   plugins: [],
 }
